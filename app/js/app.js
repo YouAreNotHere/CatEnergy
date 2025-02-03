57371417611890d3790b4706074fd725a13d15bd
YouAreNotHere <malferety@gmail.com>
2025-02-03 22:50:01 +0500

    deploy 4

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+const navMain = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs")
+
+navToggle.addEventListener("click", function(){
+  if (navMain.classList.contains("main-nav--closed")){
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+  if (navToggle.classList.contains("main-nav__toggle--opened")){
+    navToggle.classList.remove("main-nav__toggle--opened");
+    navToggle.classList.add("main-nav__toggle--closed")
+  } else {
+    navToggle.classList.add("main-nav__toggle--opened");
+    navToggle.classList.remove("main-nav__toggle--closed");
+  }
+});
